16c5ec00c91a9edd1196fe6b00e29161b29885e7
assombrance <assombrance.anderson@gmail.com>
2015-12-14 16:02:59 +0100

    Update main.c

--- Traducteur-assembleur-hexa/main.c
+++ Traducteur-assembleur-hexa/main.c
@@ -3,7 +3,7 @@
 int main (int argc, char* argv) {
   //on prend le fichier
   while (...) { //on le lit ligne par ligne
-    convMnemo(ligne); on traduit la ligne
+    convMnemo(ligne); //on traduit la ligne
   }
   return 0;
 }
