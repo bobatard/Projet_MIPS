b6931f58d83373dfd0315768f70ee66ccd4687f3
assombrance <assombrance.anderson@gmail.com>
2015-12-14 16:17:31 +0100

    Create convMnemo.c

---
+++ Traducteur-assembleur-hexa/convMnemo.c
@@ -0,0 +1,21 @@
+#include "convMnemo.h"
+
+int convMnemo(char* ligne) { //convertit une ligne en son code hexadécimal
+  int pointeur=0; //pointeur pour lire la ligne
+  int i=0,resultat;
+  char* operation[4];
+  while(...) {//va au premier non espace
+    pointeur++;
+  }
+  while(...i) {//lit le premier mot
+    operation[i]=ligne[pointeur];
+    i++;
+    pointeur++;
+  }
+  if(operation=="add"){
+    resultat=add(ligne);
+  }else if(operaton=="..."){
+    resultat=...(ligne);
+  }...else return -1;
+  return resultat;
+}
