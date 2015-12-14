fde7e643e79133e66409c388a29de2351ecd9ef4
assombrance <assombrance.anderson@gmail.com>
2015-12-14 16:02:37 +0100

    Create main.c

---
+++ Traducteur-assembleur-hexa/main.c
@@ -0,0 +1,10 @@
+#include <bibli.h>
+
+int main (int argc, char* argv) {
+  //on prend le fichier
+  while (...) { //on le lit ligne par ligne
+    convMnemo(ligne); on traduit la ligne
+  }
+  return 0;
+}
+  
