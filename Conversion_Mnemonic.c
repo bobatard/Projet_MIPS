eaf5e08fd7c43ff3670e2f337e98323f2db7c4ac
Alexandre <alexandre.eid@etu.esisar.grenoble-inp.fr>
2015-12-14 16:04:59 +0100

    Conversion des mnemonics des instructions en hexadecimal. RAISONNEMENT

---
+++ Conversion_Mnemonic.c
@@ -0,0 +1,13 @@
+/* Convertir les mneminiques des instructions assembleur 
+ * en hexadécimal. */
+
+ int ConvMnemonique(char* mnemo) {
+
+ 	/*  1) on sélectionne le mot
+ 		2) on parcours le mot 
+ 		3) on convertit chaque lettre en hexa
+ 		4) On somme
+ 		5) On renvoie le résultat
+ 									*/
+
+ }
